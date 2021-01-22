a1947f1335d5aa9b65bedceec86cabb61861996f
arianecrestani <ariane.mrv@gmail.com>
2021-01-22 15:46:40 +0100

    remove comit

--- code/index.js
+++ code/index.js
@@ -50,26 +50,19 @@ function createDivSection() {
 }
 //
 function createHomeSection() {
-    //get container
+   
     const container = document.getElementsByClassName('container')[0];
-   //get DivSection criated
     const section = createDivSection();
-    //addc section on container
     container.appendChild(section);
-    //created new element homeDiv
+
     const homeSection = document.createElement('div');
-    //created a className and id
     homeSection.className = 'divSectionHome';
     homeSection.id = 'home';
-    //addc section on homeSection
     section.appendChild(homeSection);
-    //created new img element 
+
     const homeImg = document.createElement('img');
-    //source img
     homeImg.src = "images/main3.jpg";
-    // id img
     homeImg.id ='imgHome';
-    //adcc a homeImg on homeSection
     homeSection.appendChild(homeImg);
 
     const homeTitle = document.createElement('div');
